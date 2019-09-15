028b2d4465c4be858da0ebeab8f278968b459123
SethKalkstein <sethkal7@yahoo.com>
2019-09-14 20:47:02 -0400

    basic content complete

--- contact.php
+++ contact.php
@@ -8,6 +8,7 @@
     include "resources/mainnav.php";
     include "resources/contactTransmittion.php";
 ?>
+<main>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6118.675055975244!2d-75.16417492009839!3d39.93383916336471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6c6194cea361f%3A0xa9f834d0a175373d!2sSouth%20Philadelphia%2C%20Philadelphia%2C%20PA%2019147!5e0!3m2!1sen!2sus!4v1567205320365!5m2!1sen!2sus" width="700" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
     <h3>Thnik we'd be a good fit or have a question?</h3>
     <form action="contact.php" method="POST">
@@ -20,6 +21,6 @@
     <a href="https://www.linkedin.com/in/seth-kalkstein">Linkedin</a>
     <a href="https://github.com/SethKalkstein">GitHub</a>
     <a href="mailto:seth@sethjkalkstein.com">Email</a>
-    
+</main>
 
 <?php include "resources/footer.php"; ?>
--- index.php
+++ index.php
@@ -12,7 +12,67 @@
     include "resources/header.php"; 
     include "resources/mainnav.php";
 ?>
+<main>
+    <h1></h1>
+    
+    <h2>Why do I write code?</h2>
+    <p>I am a software developer who has always loved using algorithms and data structures to improve both my work environment, and that of my colleagues.</p>
+    
+    <h2>How did I get here?</h2>
+    <p>In my past and present positions - whether as a team member, team leader, or outside advisor - I made extensive use of computer technology and pattern analysis to increase efficiency, profits, and even team communication. Over the years I found that I was most absorbed in my job when I had the opportunity to program my computer to do things such as convert data or make calculations based on conditions I gave it - so much so that I became the go-to person when colleagues needed their spreadsheets debugged! The next logical step was to dive into that state of mind and make it my full-time job.</p>
+    
+    <h2>What technologies do I use?</h2>
+    <!-- JavaScript, PHP, Linux, MySQL, Jquery, HTML5, CSS3, Ruby, Ruby on Rails, Sinatra, ActiveRecord, SQLite3, Git, GitHub, Heroku, SASS -->
+    <ul>
+        <li>JavaScript</li>
+        <li>PHP</li>
+        <li>Linux</li>
+        <li>MySQL</li>
+        <li>Jquery</li>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>Ruby</li>
+        <li>Ruby on Rails</li>
+        <li>SQLite3</li>
+        <li>ActiveRecord</li>
+        <li>Git</li>
+        <li>GitHub</li>
+        <li>SASS</li>
+    </ul>
 
-    <h1>This will be the site</h1>
+    <h2>Do I have other skills?</h2>
+    <p>Yes, I do! My background in multiple work environments has given me strong skills in the following...</p>
+    <!-- Clear Communication, Research, Team Work, Problem Solving, Public Speaking, Negotiating, Flexibility, Project Management, Training, Growth Oriented, Customer Service, Documentation, Sales, Merchandising, Oversight, Fixing Bugs -->
+    <!-- data analysis, teamwork and team-building, team communication, market research, sales, buying, negotiating, customer relations, finance, operations management, training, and thirst for product knowledge -->
+    <ul>
+        <li>Teamwork</li>
+        <li>Team Building</li>
+        <li>Clear Communication</li>
+        <li>Research</li>
+        <li>Problem Solving</li>
+        <li>Data Analysis</li>
+        <li>Patten Recognition</li>
+        <li>Public Speaking</li>
+        <li>Negotiating</li>
+        <li>Flexibility</li>
+        <li>Project Management</li>
+        <li>Training</li>
+        <li>Growth Oriented</li>
+        <li>Customer Service</li>
+        <li>Documentation</li>
+        <li>Sales</li>
+        <li>Merchandising</li>
+        <li>Oversight</li>
+        <li>Customer Relations</li>
+        <li>Thirst for Knowledge</li>
+        <li>Profit Maximization</li>
+    </ul>
+
+    <h3>Do I have other interests?</h3>
+    <p>Of course! I love cats, with a particular affinity for rescue cats that have stripes or grey tuxedos. I ride my bicycle on a daily basis, and also like to hike, ice skate, and ski. Recently I've gotten into gardening, and am proud to have kept most of my flowers, herbs, and other plants alive. (but squirrels ate my Sun Gold tomatoes) I'm always up for watching Star Trek, especially while eating a good peice of cheese and drinking some strong loose leaf tea, preferably with a striped and grey tuxedo cat at my side.</p>
+
+    <h3>Anything else?</h3>
+    <p>Head on over to my <a href="projects/index.php">projects page</a> to see a few selected projects including the code for this site. If you want to know any more about me or think that I would be a good fit for your team, feel free to <a href="contact.php">contact</a> me.</p>
+</main>
 
 <?php include "resources/footer.php" ?>
