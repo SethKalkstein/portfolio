bea2fd9df8cbbda4f8ed97868ef845cb2f9d35df
SethKalkstein <sethkal7@yahoo.com>
2020-01-07 21:20:26 -0500

    changed verbage and resume to reflect new skills

--- index.php
+++ index.php
@@ -14,87 +14,92 @@
 ?>
 <main id="about-container">
 
-    <section class="about">
-        <h2>Why do I develop software?</h2>
-        <img src="images/headShot.jpg" alt="I found a decent looking picture of myself, so here it is!">
-        <p>I have always loved using computers for automating solutions to improve both my work environment, and that of my colleagues. </p>
-        <p>As a manager and consultant in the gourmet grocery industry I found myself writing algorithms which included conditional statements to trigger math and string functions for data conversion using data structures resembling anything from booleans to multi-dimensional arrays. Dynamically creating multiple spreadsheet formats for both people and machines, and dynamically calculating data from levels of store coverage to hours worked with breaks to spending or financial periods, made my life much easier than manually sifting for special cases on the computer (or worse yet, by hand), not to mention it was much more fun!</p>
-        <p>Even as a real estate agent I found ways to incorporate programming logic in my spreadsheets. My favorite example was writing filter algorithms and string functions in mail-merge spreadsheets in order to extract names and eliminate duplicates, thereby allowing me to inform hundreds of interested agents of changes to the status of my clients' property in an unobtrusive way.</p>
-    </section>
+<section class="about">
+    <h2>Why do I develop software?</h2>
+    <img src="images/headShot.jpg" alt="I found a decent looking picture of myself, so here it is!">
+    <p>I have always loved using computers for automating solutions to improve both my work environment, and that of my colleagues. </p>
+    <p>As a manager and consultant in the gourmet grocery industry I found myself writing algorithms which included conditional statements to trigger math and string functions for data conversion using data structures resembling anything from booleans to multi-dimensional arrays. Dynamically creating multiple spreadsheet formats for both people and machines, and dynamically calculating data -- from levels of store coverage, to hours worked while compensating for unpaid break time, to spending, to financial periods -- made my life much easier than manually sifting for special cases on the computer (or worse yet, by hand), not to mention it was much more fun!</p>
+    <p>Even as a real estate agent I found ways to incorporate programming logic in my spreadsheets. My favorite example was writing filter algorithms and string functions in mail-merge spreadsheets in order to extract names and eliminate duplicates, thereby allowing me to inform hundreds of interested agents of changes to the status of my clients' property in an unobtrusive way.</p>
+</section>
 
-    <section class="about">
-        <h2>How did I get here?</h2>
-        <img src="images/momJonEphesus2.jpg" alt="Image for 'how did I get here?' (my mother is in this picture.)">
-        <p>Not only do I love to build code, but clear communication with clients, customers, colleagues, superiors and subordinates is second nature to me from my background in retail food management and real estate sales. So, with me, you get the complete package - the technical critical thinking skills that it takes to find solutions with computers, and the people skills to work with fellow humans or craft solutions for them.</p>
-        <p>In my past and present positions - whether as a team member, team leader, or outside advisor - I made extensive use of computer technology and pattern analysis to increase efficiency, profits, and even team communication. Over the years I found that I was most absorbed in my job when I had the opportunity use my computer to automate, convert data, or make calculations based on conditions I gave it - so much so that I became the go-to person when colleagues needed their spreadsheets debugged! The next logical step was to dive into that state of mind and make it my full-time job. </p>
-        <p>While still working as a real estate agent in late 2018, albeit with a reduced schedule, I completed a full-time on-site immersive full-stack boot camp in Philadelphia called New York Code and Design Academy, where we focused on JavaScript, Ruby, Ruby on Rails, HTML5, and CSS3. (See my <a href="resume.pdf">resume</a> for a complete list of my tech, skills, and concepts) Inevitably, there were still bills to pay after the boot camp ended, so I increased my time spent working in real estate through the summer. In July I was finally able to start shifting my main focus back to writing and learning code, and was able to ramp up my learning even more in the fall. </p>
-    </section>
+<section class="about">
+    <h2>How did I get here?</h2>
+    <img src="images/momJonEphesus2.jpg" alt="Image for 'how did I get here?' (my mother is in this picture.)">
+    <p>Not only do I love to build code, but clear communication with clients, customers, colleagues, superiors and subordinates is second nature to me from my background in retail food management and real estate sales. So, with me, you get the complete package - the technical critical thinking skills that it takes to find solutions with computers, and the people skills to work with fellow humans or craft solutions for them.</p>
+    <p>In my past and present positions - whether as a team member, team leader, or outside advisor - I made extensive use of computer technology and pattern analysis to increase efficiency, profits, and even team communication. Over the years I found that I was most absorbed in my job when I had the opportunity use my computer to automate, convert data, or make calculations based on conditions I gave it - so much so that I became the go-to person when colleagues needed their spreadsheets debugged! The next logical step was to dive into that state of mind and make it my full-time job. </p>
+    <p>While still working as a real estate agent in late 2018, albeit with a reduced schedule, I completed a full-time on-site immersive full-stack boot camp in Philadelphia called New York Code and Design Academy, where we focused on JavaScript, Ruby, Ruby on Rails, HTML5, and CSS3. (See my <a href="resume.pdf">resume</a> for a complete list of my tech, skills, and concepts) Inevitably, there were still bills to pay after the boot camp ended, so I increased my time spent working in real estate through the summer. In July I was finally able to start shifting my main focus back to writing and learning code, and was able to ramp up my learning even more in the fall. </p>
+</section>
 
-    <section class="about">
-        <h2>Where am I now?</h2>
-        <img src="images/colorfulCode2.jpg" alt="generic picture of code">
-        <p>Since July, I reviewed and solidified the concepts I learned at boot camp, taught myself LAMP stack, and had fun with some coding challenges. Currently, I’m supplementing my practical programming skills by further exploring traditional computer science concepts. I’m applying many of those concepts and expanding my object oriented programming skills by teaching myself Java. In JavaScript, I’ve been delving deeper into concurrency, and learned more ES6. I plan to start learning Node.js within the next month. In the next year I’d like to learn mobile development, and further down the road, have some fun tinkering with wearable development. </p>
-        <p>Real life challenges that involve critical thinking, and my love of finding solutions to those challenges, are what brought me to change my career to software development in the first place. I’m excited to get back to working in a team, solving problems for the real world, and learning from my colleague’s solutions (and hopefully in the near future, having them learn from mine) - as that is the environment where my learning and contributions accelerate the fastest. Isn’t a symbiotic work environment a beautiful thing!? I hope to find an entry-level position, apprenticeship that leads to a full-time position, or consultant role that will lead to a full time position, where I can thrive, help grow the company and form a working relationship that will last a long time. Take a look on my <a href=””>resume</a> and you’ll see that I’m a big fan of finding a good work situation and staying there!</p>
-    </section>
+<section class="about">
+    <h2>Where am I now?</h2>
+    <img src="images/colorfulCode2.jpg" alt="generic picture of code">
+    <p>Since July, I reviewed and solidified the concepts I learned at boot camp, taught myself LAMP stack, and had fun with some coding challenges. Currently, I’m supplementing my practical programming skills by further exploring traditional computer science concepts. I’m applying many of those concepts and expanding my object oriented programming skills by teaching myself Java. In JavaScript, I’ve been delving deeper into concurrency, ES6 concepts and Typescript. Over the past couple of months I’ve learned Node, along with Express and Mongo, and just started learning Angular. In the next year I’d like to learn mobile development, and further down the road, have some fun tinkering with wearable development. </p>
+    <p>Real life challenges that involve critical thinking, and my love of finding solutions to those challenges, are what brought me to change my career to software development in the first place. I’m excited to get back to working in a team, solving problems for the real world, and learning from my colleague’s solutions (and hopefully in the near future, having them learn from mine) - as that is the environment where my learning and contributions accelerate the fastest. Isn’t a symbiotic work environment a beautiful thing!? I hope to find an entry-level position, apprenticeship that leads to a full-time position, or consultant role that will lead to a full time position, where I can thrive, help grow the company and form a working relationship that will last a long time. Take a look on my <a href=””>resume</a> and you’ll see that I’m a big fan of finding a good work situation and staying there!</p>
+</section>
 
-    <section class="about">
-        <h2>What technologies do I use?</h2>
-        <ul>
-            <li>JavaScript</li>
-            <li>PHP</li>
-            <li>Linux</li>
-            <li>MySQL</li>
-            <li>Jquery</li>
-            <li>HTML5</li>
-            <li>CSS3</li>
-            <li>Ruby</li>
-            <li>Ruby on Rails</li>
-            <li>SQLite3</li>
-            <li>ActiveRecord</li>
-            <li>Git</li>
-            <li>GitHub</li>
-            <li>SASS</li>
-        </ul>
-    </section>
-    
-    <section class="about">
-    <h2>Do I have other skills?</h2>
-        <p>Yes, I do! My background in multiple work environments has given me strong skills in the following...</p>
-        <ul>
-            <li>Teamwork</li>
-            <li>Team Building</li>
-            <li>Clear Communication</li>
-            <li>Research</li>
-            <li>Problem Solving</li>
-            <li>Data Analysis</li>
-            <li>Pattern Recognition</li>
-            <li>Public Speaking</li>
-            <li>Negotiating</li>
-            <li>Flexibility</li>
-            <li>Project Management</li>
-            <li>Training</li>
-            <li>Growth Oriented</li>
-            <li>Customer Service</li>
-            <li>Documentation</li>
-            <li>Sales</li>
-            <li>Merchandising</li>
-            <li>Oversight</li>
-            <li>Customer Relations</li>
-            <li>Thirst for Knowledge</li>
-            <li>Profit Maximization</li>
-        </ul>
-    </section>
+<section class="about">
+    <h2>What technologies do I use?</h2>
+    <ul>
+<li>Java</li>
+        <li>JavaScript</li>
+        <li>PHP</li>
+<li>Ruby</li>
+        <li>HTML5</li>
+        <li>CSS3</li>
+<li>Typescript</li>
+<li>SASS</li>
+<li>Node</li>
+<li>Express</li>
+        <li>Ruby on Rails</li>
+        <li>Jquery</li>
+        <li>MySQL</li>
+        <li>SQLite3</li>
+<li>MongoDB</li>
+        <li>ActiveRecord</li>
+        <li>Linux</li>
+        <li>Git</li>
+        <li>GitHub</li>
+    </ul>
+</section>
 
-    <section class="about">
-        <h2>Do I have other interests?</h2>
-        <img src="images/kitties.jpg" alt="A striped cat and grey tuxedo cat">
-        <p>Of course! I love cats, with a particular affinity for rescue cats who have stripes or grey tuxedos. I ride my bicycle on a daily basis, and I also like to hike, ice skate, and ski. Recently I've gotten into gardening, and am proud to have kept most of my flowers, herbs, and other plants alive - despite squirrels eating my Sun Gold tomatoes last summer! I'm always up for watching Star Trek, especially while eating a good piece of cheese and drinking some strong loose leaf tea, preferably with a striped cat and grey tuxedo cat by my side.</p>
-    </section>
+<section class="about">
+<h2>Do I have other skills?</h2>
+    <p>Yes, I do! My background in multiple work environments has given me strong skills in the following...</p>
+    <ul>
+        <li>Teamwork</li>
+        <li>Team Building</li>
+        <li>Clear Communication</li>
+        <li>Research</li>
+        <li>Problem Solving</li>
+        <li>Data Analysis</li>
+        <li>Pattern Recognition</li>
+        <li>Public Speaking</li>
+        <li>Negotiating</li>
+        <li>Flexibility</li>
+        <li>Project Management</li>
+        <li>Training</li>
+        <li>Growth Oriented</li>
+        <li>Customer Service</li>
+        <li>Documentation</li>
+        <li>Sales</li>
+        <li>Merchandising</li>
+        <li>Oversight</li>
+        <li>Customer Relations</li>
+        <li>Thirst for Knowledge</li>
+        <li>Profit Maximization</li>
+    </ul>
+</section>
 
-    <section class="about">
-        <h2>Anything else?</h2>
-        <p>Head on over to my <a href="projects/index.php">Projects</a> page to see a few selected projects including the code for this site. If you want to know more about me and have some questions or think that I would be a good fit for your team, feel free to <a href="contact.php">contact</a> me.</p>
-    </section>
+<section class="about">
+    <h2>Do I have other interests?</h2>
+    <img src="images/kitties.jpg" alt="A striped cat and grey tuxedo cat">
+    <p>Of course! I love cats, with a particular affinity for rescue cats who have stripes or grey tuxedos. I ride my bicycle on a daily basis, and I also like to hike, ice skate, and ski. Recently I've gotten into gardening, and am proud to have kept most of my flowers, herbs, and other plants alive - despite squirrels eating my Sun Gold tomatoes last summer! I'm always up for watching Star Trek, especially while eating a good piece of cheese and drinking some strong loose leaf tea, preferably with a striped cat and grey tuxedo cat by my side.</p>
+</section>
+
+<section class="about">
+    <h2>Anything else?</h2>
+    <p>Head on over to my <a href="projects/index.php">Projects</a> page to see a few selected projects including the code for this site. If you want to know more about me and have some questions or think that I would be a good fit for your team, feel free to <a href="contact.php">contact</a> me.</p>
+</section>
 </main>
 
 <?php include "resources/footer.php" ?>
